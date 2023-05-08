81a99806980e14ff0d1bbbbb3de3c73c0c066496
arunadiya <113434274+arunadiya@users.noreply.github.com>
2023-05-08 15:48:05 +0530

    Add files via upload

---
+++ Educationalloan/src/main/java/com/example/Educationalloan/Repository/LoanApplicationRepo.java
@@ -0,0 +1,11 @@
+package com.example.Educationalloan.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Educationalloan.Model.LoanApplicationModel;
+
+@Repository
+public interface LoanApplicationRepo extends JpaRepository <LoanApplicationModel,Integer>{
+
+}
---
+++ Educationalloan/src/test/java/com/example/Educationalloan/EducationalloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Educationalloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
